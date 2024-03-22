a5925788e2a1c0a8a7645ce15f9cdc21ce6de8a8
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-22 12:49:15 -0300

    Setter injection

--- src/main/java/com/luv2code/springcoredemo/App.java
+++ src/main/java/com/luv2code/springcoredemo/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcoredemoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringcoredemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
