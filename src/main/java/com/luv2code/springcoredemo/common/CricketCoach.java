05d63491be99925f28965a26bf7c916eecfc685e
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-22 14:51:26 -0300

    Bean Lifecycle Methods

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,21 +1,34 @@
 package com.luv2code.springcoredemo.common;
 
 import com.luv2code.springcoredemo.common.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In constructor " + getClass().getSimpleName());
     }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff():" + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanStuff():" + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
-
         return "Practice fast bowling for 15 minutes";
     }
 };
