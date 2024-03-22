98da333560269465c024739df97eb62122608b6e
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-22 13:08:37 -0300

    Created Baseball Coach class

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public void demoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
